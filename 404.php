aa61ed4ea834274e1fff224c8ae77550f9bc5aee
Oropesa <coropesa.gcc@gmail.com>
2013-12-16 20:11:27 +0000

    update template 404

--- 404.php
+++ 404.php
@@ -2,7 +2,25 @@
 
 <?php get_header(); ?>
 
+    <section id="primary" role="main">
 
-404!
+        <article id="page-404" class="page not-found">
+
+            <header class="entry-header">
+
+                <h1 class="entry-title">Error 404. No encontrado</h1>
+
+            </header><!-- .entry-header -->
+
+            <section class="article-content">
+
+                <p>En serio, En este lugar no hay nada y lo que usted busca ya no existe (o de hecho, nunca ha existido).</p>
+                <p>Sin embargo, aquí hay cosas interesantes. ¡Echa un vistazo!</p>
+
+            </section><!-- .entry-content -->
+
+        </article><!-- #page-404 -->
+
+    </section>
 
 <?php get_footer(); ?>
