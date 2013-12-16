367323730b72cf0087d94a328091eba0f8cb2a65
Oropesa <coropesa.gcc@gmail.com>
2013-12-16 20:13:37 +0000

    fix content of template 404

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@
 
             <section class="article-content">
 
-                <p>En serio, En este lugar no hay nada y lo que usted busca ya no existe (o de hecho, nunca ha existido).</p>
+                <p>En serio, En este lugar no hay nada y lo que usted busca ya no existe (o incluso, nunca ha existido).</p>
                 <p>Sin embargo, aquí hay cosas interesantes. ¡Echa un vistazo!</p>
 
             </section><!-- .entry-content -->
