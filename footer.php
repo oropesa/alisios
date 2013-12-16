d21e1f67b7c319808d69e2e5da2afa4bab0a5784
Oropesa <coropesa.gcc@gmail.com>
2013-12-16 18:49:19 +0000

    add footer
    * footer
    ** copyright & name
    
    * body
    ** wraps ending

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+		</div><!-- /.content-wrapper -->
+
+		<footer class="footer" role="contentinfo">
+
+			<div class="footer-content">
+
+                <p>&copy; 2013 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php echo esc_attr( get_bloginfo( 'name' ) ); ?></a>.</p>
+
+			</div><!-- /.footer-content -->
+
+		</footer>
+
+	</div><!-- /.inner-wrap -->
+
+</div><!-- /.outer-wrap -->
+
+<!-- BEG wp_footer -->
+<?php wp_footer(); ?>
+<!-- END wp_footer -->
+
+<?php echo '<!-- Number of Queries:'; echo (get_num_queries()); echo '   Seconds: '; timer_stop(1); echo '-->'; echo "\n"; ?>
+
+</body>
+</html>
