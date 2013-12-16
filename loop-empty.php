9c50120821a3d30ac525850fbaf6591ae0aed3a1
Oropesa <coropesa.gcc@gmail.com>
2013-12-16 19:35:20 +0000

    add template loop-empty.php

---
+++ loop-empty.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+<div id="content" class="post-wrap">
+
+    <article id="post-0" class="post no-results not-found">
+
+        <header class="entry-header">
+
+            <h1 class="entry-title">No se ha encontrado nada</h1>
+
+        </header><!-- .entry-header -->
+
+        <section class="entry-content">
+
+            <p>Lo sentimos, pero todavía no hay ninguna entrada relativa a lo que busca.</p>
+
+        </section><!-- .entry-content -->
+
+    </article><!-- #post-0 -->
+
+</div><!--/.post-wrap-->
