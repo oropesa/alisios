e832e9d1379964e3722c9902e6840e17787353d5
Oropesa <coropesa.gcc@gmail.com>
2013-12-19 18:20:45 +0000

    add functions.php extensions basic structure

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Load Framework
+ */
+require_once( get_template_directory() . '/framework/alisios-functions-init.php' );
