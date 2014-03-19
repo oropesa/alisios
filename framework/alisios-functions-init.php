30bfa8d148fc0a69730eb4287c5daf3290c6508d
Oropesa <coropesa.gcc@gmail.com>
2014-03-19 15:00:01 +0000

    Define Constant Vars in alisios-functions-init.php

--- framework/alisios-functions-init.php
+++ framework/alisios-functions-init.php
@@ -1,15 +1,32 @@
 <?php
 
-$extensionsDirectory = get_template_directory() . '/framework/extensions';
+// ~~ VARS
 
-/* LESS */
-require_once( $extensionsDirectory . '/less/init.php' );
+//bool
+define('ALISIOS_USE_LESS', true);
 
-/* Bootstrap */
-require_once( $extensionsDirectory . '/bootstrap/init.php' );
+//textdomain
+define('ALISIOS_I18N', 'alisios');
 
-/* Languages */
-require_once( $extensionsDirectory . '/languages/init.php' );
+//urls
+define('ALISIOS_URL_STYLE',     get_stylesheet_uri());
+define('ALISIOS_URL_THEME',     get_template_directory_uri());
+define('ALISIOS_URL_FRAMEWORK', ALISIOS_URL_THEME . '/framework');
+define('ALISIOS_URL_FONTS',     ALISIOS_URL_FRAMEWORK . '/fonts');
+define('ALISIOS_URL_JS_LIB',    ALISIOS_URL_FRAMEWORK . '/js/jslib');
+define('ALISIOS_URL_JS',        ALISIOS_URL_FRAMEWORK . '/js');
 
-/* Sidebars */
-require_once( $extensionsDirectory . '/sidebars/init.php' );
+//dir
+define('ALISIOS_DIR_THEME',     get_template_directory());
+define('ALISIOS_DIR_FRAMEWORK', ALISIOS_DIR_THEME . '/framework');
+define('ALISIOS_DIR_INC',       ALISIOS_DIR_FRAMEWORK . '/includes');
+
+// ~~ LOAD FUNCTIONS
+
+require_once( ALISIOS_DIR_FRAMEWORK . '/alisios-functions-setup.php' );
+
+require_once( ALISIOS_DIR_FRAMEWORK . '/alisios-functions-head.php' );
+
+// ~~ LOAD ACTIONS
+
+require_once( ALISIOS_DIR_FRAMEWORK . '/alisios-actions.php' );
