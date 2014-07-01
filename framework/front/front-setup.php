659c5507b4d778a0276fc4cebc85e50df9b52362
Oropesa <coropesa.gcc@gmail.com>
2014-07-01 10:42:58 +0100

    now the get_[part]_class functions code is here, in the function array_alisios_class() and it's possible to edit, add & remove classes

--- framework/front/front-setup.php
+++ framework/front/front-setup.php
@@ -2,11 +2,33 @@
 
 class AlisiosFrontSetup {
 
-    /*
-     * LANGUAGES
+    /* LANGUAGES
      */
     public static function load_i18n() {
         load_theme_textdomain( ALISIOS_I18N, ALISIOS_DIR_THEME . '/languages' );
     }
 
+}
+
+/* Extra Functions
+ */
+
+function string2array($string) {
+    $class = array();
+
+    if(is_string($string))
+        $class = preg_split('#[\s,]+#', $string);
+
+    $class = array_map( 'esc_attr', $class );
+
+    return $class;
+}
+
+function array_alisios_class($classes, $addClass, $removeClass) {
+    //add
+    $classes = array_merge($classes, string2array($addClass));
+    //remove
+    $classes = array_diff($classes, string2array($removeClass));
+
+    return $classes;
 }
